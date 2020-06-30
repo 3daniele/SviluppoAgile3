a0aa4ac67a95b5cad250a27f65d93468e526c1de
Daniele <30625283+3daniele@users.noreply.github.com>
2020-06-30 18:38:11 +0200

    bug fix

--- Project/resources/views/layouts/app.blade.php
+++ Project/resources/views/layouts/app.blade.php
@@ -50,7 +50,7 @@
                             @endif
                         @else
                         <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('new_party') }}">New  party</a>
+                                    <a class="nav-link" href="{{ route('hosting') }}">New  party</a>
                     </li>
                            <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
